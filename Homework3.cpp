7dbefe40c06058e7008aeee7f7c8bc1fae61a1e2
SunisaThakphirom <68006388+SunisaThakphirom@users.noreply.github.com>
2020-08-24 17:15:08 +0700

    Add files via upload

--- Homework3.cpp
+++ Homework3.cpp
@@ -2,28 +2,21 @@
 using namespace std;
 int main()
 {
-    float first,second,third,mid_term1,mid_term2,final1,final2,total,quizz_final;
-    cout << "============QUIZZES============="<<endl;
-    cout << "Enter first quizz (10) : ";
-    cin >> first;
-    cout << "Enter second quizz (10) : ";
-    cin >> second;
-    cout << "Enter third quizz (10) : ";
-    cin >> third ;
-    cout << "============MID-TERM============"<<endl;
-    cout << "Enter mid-term (40) : ";
-    cin >> mid_term1;
-    cout << "============FINAL==============="<<endl;
-    cout << "Enter final (50) : ";
-    cin >> final1;
-    quizz_final= (first+second+third)/3;
-    cout << "Quizz final : " <<  quizz_final << endl;
-    cout << "Mid-term : ";
-    cin >> mid_term2;
-    cout << "Final : ";
-    cin >> final2;
-    total = mid_term2+final2;
-    cout << "Total : "<< quizz_final + total <<endl;
-    cout << "Your score is " << ((total>=50)? "Pass." : "FALL") <<endl;
+    char grade;
+    int i = 0,total = 0;
+    float GPA = 0;
+    do {
+        cout << "Enter the letter grade ( Enter 'X' to Exit ) " << endl;
+        cin >> grade;
+        if ( grade == 'a' || grade == 'A') total += 4 ;
+        else if( grade == 'b' || grade == 'B') total += 3 ;
+        else if( grade == 'c' || grade == 'C') total += 2 ;
+        else if( grade == 'd' || grade == 'D') total += 1 ;
+        else if( grade == 'f' || grade == 'F') total += 0 ;
+        else if( grade == 'x' || grade == 'X') break ;
+        i++;
+    } while ( grade != 'x' && grade != 'X' );
+    cout << " Total Grade Points : " << total << endl;
+    cout << " GPA : " << (float) total/i << endl;
     return 0;
 }
